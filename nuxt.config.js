f2320a6b523736028047c84ce9bbb222d4936089
nikhil18-9192 <nikhilkorane08@gmail.com>
2021-07-20 14:41:19 +0530

    typo

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
 
   head: {
-    title: 'jack & jill',
+    title: 'Jack & Jill',
     htmlAttrs: {
       lang: 'en',
     },
