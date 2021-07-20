ae5763ca93c35f1b7a72940acde8f040ed50f985
nikhil18-9192 <nikhilkorane08@gmail.com>
2021-07-20 14:31:53 +0530

    change site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
 
   head: {
-    title: 'jj-nuxt',
+    title: 'jack & jill',
     htmlAttrs: {
       lang: 'en',
     },
